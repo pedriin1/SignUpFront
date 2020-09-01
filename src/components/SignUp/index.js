a750f5731bd1a3cf41e6497230c45edd201d6628
pedriin1 <frigopedro@icloud.com>
2020-09-01 19:06:46 -0300

    All right

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,24 +1,51 @@
 import React, { Component } from 'react';
 import signUp2 from '../../img/signUp.png';
 import './styles.css'
-import signUp from '../../img/lock.png'
 
 class SignUp extends Component {
     constructor(props) {
         super(props);
         this.state = {
+
             signPassword1: '',
             signPassword2: '',
             verificacao: '',
+            flag: '',
+
+            email: '',
+            first_name: '',
+            last_name: '',
+            fone: '',
             password: '',
-            flag: ''
+            img: ''
+
         }
         this.signSubmit = this.signSubmit.bind(this);
 
     }
-    signSubmit(e){
-        if(this.state.flag === '1'){
+    signSubmit(e) {
+        if (this.state.flag === '1') {
             console.log('oi')
+            var url = '';
+            const requestOptions = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    email: this.state.email, first_name: this.state.first_name,
+                    last_name: this.state.last_name, fone: this.state.fone, password: this.state.password
+                })
+            };
+            requestOptions.headers["Authorization"] = "Token 89cc7e6ffc781d3dee5507f531013bcd0c9009ba"
+            fetch(url, requestOptions)
+                .then(response => response.json())
+                .then(data => {
+                    localStorage.setItem('token', data.token);
+                    if (localStorage.getItem('token') === 'undefined') {
+                        localStorage.removeItem('token')
+                        this.setState({ erro: true })
+                    }
+                    this.setState({ token: data.token });
+                })
         }
         e.preventDefault()
     }
@@ -33,30 +60,42 @@ class SignUp extends Component {
                     </div>
                     <form className="signUp" onSubmit={this.signSubmit}>
                         <input type="email" className="emailInput" placeholder='Digite um email' required />
+
+
                         <div className='firstAndLastName'>
-                            <input type="text" className='firstNameSign' placeholder="Primeiro Nome" />
-                            <input type="text" className='lastNameSign' placeholder="Segundo Nome" />
+                            <input type="text" className='firstNameSign' placeholder="Primeiro Nome"
+                                onChange={(e) => { this.setState({ first_name: e.target.value }) }} />
+                            <input type="text" className='lastNameSign' placeholder="Segundo Nome"
+                                onChange={(e) => { this.setState({ last_name: e.target.value }) }} />
                         </div>
-                        <input type="text" className='nameSign' placeholder="Telefone" required/>
+
+
+                        <input type="text" className='nameSign' placeholder="Telefone" required
+                            onChange={(e) => { this.setState({ fone: e.target.value }) }} />
+
 
                         <input type="password" className="passwordInput" placeholder='Digite uma senha' required
                             onChange={(e) => { this.setState({ signPassword1: e.target.value }) }} />
 
+
                         <input type="password" className="password2Input" placeholder='Repita sua senha' required
-                            onChange={async (e) =>  {this.setState({ signPassword2: e.target.value })
+                            onChange={async (e) => {
+                                this.setState({ signPassword2: e.target.value })
 
-                            if(this.state.signPassword1 !== this.state.signPassword2){
+                                if (this.state.signPassword1 !== this.state.signPassword2) {
 
-                              await this.setState({verificacao : 'As senhas são diferentes'})
-                              await this.setState({flag : ''})
-                              console.log(this.state.signPassword1 + ' ' + this.state.signPassword2)
+                                    await this.setState({ verificacao: 'As senhas são diferentes' })
+                                    await this.setState({ flag: '' })
+                                    console.log(this.state.signPassword1 + ' ' + this.state.signPassword2)
 
-                            } if(this.state.signPassword1 === this.state.signPassword2){
-                                await this.setState({verificacao : ''})
-                                await this.setState({flag : '1'})
-                            }}} />
+                                } if (this.state.signPassword1 === this.state.signPassword2) {
+                                    await this.setState({ verificacao: '' })
+                                    await this.setState({ flag: '1' })
+                                }
+                            }} />
 
                         <p className='verify'>{this.state.verificacao}</p>
+
                         <input type="file" id="file" className='inputArquivo' />
 
                         <div className="custom-control custom-checkbox custom-control-inline">
